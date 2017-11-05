9c7931d9ebb1b348fc57b8dbcf6a907e8c861d5a
Alesya <saauser77@mail.ru>
2017-11-06 00:24:18 +0300

    priority-queue/feature:remove

--- src/max-heap.js
+++ src/max-heap.js
@@ -8,27 +8,38 @@ this.length=0;
 	}
 
 	push(data, priority) {
-this.length++;
+		this.insertNode(new Node(data,priority));
+		this.shiftNodeUp(new Node(data,priority));
+		this.parentNodes.unshift(new Node(data,priority));
+//this.length++;
 	}
 
 	pop() {
 this.length--;
+if(this.heap){
+//this.detachRoot();
+//this.restoreRootFromLastInsertedNode(this.detachRoot());
+//this.shiftNodeDown(this.heap.root);
+return this.root.data;
 
 	}
+}
 
 	detachRoot() {
-
+let detachedroot=this.root;
+this.root=null;
 		//if(this.root==undefined){
 			//return null
 		//}
 //let r= this.root.data;
 
-//this.parentNodes.shift();
-return this.root;
+this.parentNodes.shift();
+return detachedroot;
 	}
 
 	restoreRootFromLastInsertedNode(detached) {
-
+//this.root=detached;//this.parentNodes[this.parentNodes.length-1];
+//this.parentNodes.pop();
 	}
 
 	size() {
@@ -40,6 +51,8 @@ return this.length==0;
 	}
 
 	clear() {
+		this.root=null;
+		this.parentNodes=[];
 this.length=0;
 	}
 
@@ -47,6 +60,15 @@ this.length=0;
 		this.length++;
 if(this.heap==null){
 	this.root=node;
+
+}
+let layer=Math.log2(1+this.parentNodes.length);
+
+	if (node.priority>this.parentNodes[0]){
+		this.parentNodes.unshift(node.priority);
+	}
+	for(let i=0;i<layer;i++){
+		if(node.priority>this.parentNodes[2*i]){node.priority=this.parentNodes[2*i]}
 }
 	}
 
--- src/node.js
+++ src/node.js
@@ -5,20 +5,33 @@ class Node {
  		this.parent=null;
 		this.left=null;
 		this.right = null;
-	//	this.child=null;
+
 	}
 
 	appendChild(node) {
+		//if(this.left==0){this.left=node};
+		//if(this.left &&(this.right==0)){this.right=node};
 
 			}
 
 
 	removeChild(node) {
+		if(node.parent){
+			
+		}
+//if(node==this.left)
+//	this.left=null;
+//if(node==this.right)
+	//this.right=null;
 
-	}
+
+//if((node!=this.left)&&(node!=this.right))
+//throw ERROR('cantremove');
+
+}
 
 	remove() {
-
+//this.parent.removeChild(this);
 	}
 
 	swapWithParent() {
